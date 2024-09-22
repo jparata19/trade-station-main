7dd70ddeffe98365be1e08ea0cacfaca256d25ba
Jose Pablo Arata <josepablo191290@gmail.com>
2024-09-22 15:59:00 -0600

    Update main.tf
    update

--- main/main.tf
+++ main/main.tf
@@ -1,14 +1,15 @@
 provider "aws" {
     region = "us-west-2"
 
-    default_tags{
+    default_tags  {
+    tags = {
         environment = "DEV"
         application = "TradeStation-test"
+        }
     }
 }
 
 module "trate-station-test" {
-#    source = "https://github.com/jparata19/trade-station-common-modules//common-modules?ref=main"
-    source = ""
+    source = "https://github.com/jparata19/trade-station-common-modules//common-modules?ref=main"
     bucket_name = "bull-media"
 }
